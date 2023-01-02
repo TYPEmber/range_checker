0cec2cf5db2afbe4a1287f7adc86a38768afae97
TYPEmber <nono02200059@gmail.com>
2023-01-02 18:34:08 +0800

    add basic test

--- examples/simple_example.rs
+++ examples/simple_example.rs
@@ -36,3 +36,76 @@ struct TestStruct {
     #[fallback(|x| x - 5)]
     v3: isize,
 }
+
+#[cfg(test)]
+pub mod test {
+    use super::*;
+
+    #[derive(Debug, Default, Check)]
+    struct TestCheckStruct {
+        #[range(..=5)]
+        #[range(20..)]
+        #[filter(|x| x % 2 != 0)]
+        #[fallback(255)]
+        pub v0: u8,
+        #[range(-1.0..=5.0)]
+        #[fallback(3.1)]
+        v1: f32,
+        #[filter(|x| x > 8.0)]
+        #[fallback(9.9)]
+        v2: f64,
+        #[range(..-1)]
+        #[fallback(|x| x - 5)]
+        v3: isize,
+    }
+
+    #[test]
+    fn basic_check_Check() {
+        let mut a = TestCheckStruct {
+            v0: 20,
+            v1: -3.0,
+            v3: 1,
+            ..Default::default()
+        };
+    
+        let _ = dbg!(a.check_with_fallback());
+    
+        dbg!(&a);
+    
+        let _ = dbg!(a.check());
+    }
+
+    #[derive(Debug, Default, CheckVerbose)]
+    struct TestCheckVerboseStruct {
+        #[range(..=5)]
+        #[range(20..)]
+        #[filter(|x| x % 2 != 0)]
+        #[fallback(255)]
+        pub v0: u8,
+        #[range(-1.0..=5.0)]
+        #[fallback(3.1)]
+        v1: f32,
+        #[filter(|x| x > 8.0)]
+        #[fallback(9.9)]
+        v2: f64,
+        #[range(..-1)]
+        #[fallback(|x| x - 5)]
+        v3: isize,
+    }
+
+    #[test]
+    fn basic_check_CheckVerbose() {
+        let mut a = TestCheckVerboseStruct {
+            v0: 20,
+            v1: -3.0,
+            v3: 1,
+            ..Default::default()
+        };
+    
+        let _ = dbg!(a.check_with_fallback());
+    
+        dbg!(&a);
+    
+        let _ = dbg!(a.check());
+    }
+}
