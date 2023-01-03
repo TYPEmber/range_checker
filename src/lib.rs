151d3feabf227b5584c9f9fed75dd81175b60a0c
TYPEmber <nono02200059@gmail.com>
2023-01-03 19:41:53 +0800

    prepare to publish dependency crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,32 @@
-pub use range_checker_basic::Check;
 pub use range_checker_macro::Check;
-
-pub use range_checker_basic::CheckVerbose;
-pub use range_checker_basic::Error;
 pub use range_checker_macro::CheckVerbose;
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("check failed! check_statement: {check_statement}, failed value: {ident} = {value}")]
+    CheckFailed {
+        ident: String,
+        value: String,
+        check_statement: String,
+    },
+    #[error("fallback success! check_statement: {check_statement}, failed value: {ident} = {value}, fallback to: {fallback}")]
+    Fallback {
+        ident: String,
+        value: String,
+        check_statement: String,
+        fallback: String,
+    },
+}
+
+pub trait Check {
+    fn check(&self) -> Result<(), ()>;
+    fn check_with_fallback(&mut self) -> Result<(), ()>;
+}
+
+pub trait CheckVerbose {
+    fn check(&self) -> Result<(), Vec<Error>>;
+    fn check_with_fallback(&mut self) ->  Result<Vec<Error>, Vec<Error>>;
+}
+
